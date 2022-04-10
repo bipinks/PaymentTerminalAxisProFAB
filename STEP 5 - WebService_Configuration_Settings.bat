e5c2bada4bbedd1353e5e7dcd363e24baa2d438c
Bipin <mr.bipinks@gmail.com>
2022-04-11 01:34:01 +0400

    Webservice configuration settings API is now automated with batch script

--- STEP 5 - WebService_Configuration_Settings.bat
+++ STEP 5 - WebService_Configuration_Settings.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SET /P WS_RUNNING_PORT=Enter webservice running PORT:
-SET /P COM_PORT_NAME=Enter COM Port in which the device is connected:
+SET /P COM_PORT_NAME=Enter the COM Port name in which the device is connected:
 
 curl -X POST http://localhost:%WS_RUNNING_PORT%/FABECRWeb/ECRWebService.asmx/Web_SaveSettings -H ^
     "Content-Type: application/x-www-form-urlencoded" -d ^
